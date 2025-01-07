8cf39bdff7fd5419e4d1555e4e335f3443044c9b
alexrowe99 <arowe890@gmail.com>
2025-01-08 09:59:18 +1030

    added leaf nodes

---
+++ src/test_leafnode.py
@@ -0,0 +1,24 @@
+from unittest import TestCase, main
+
+from leafnode import LeafNode
+from unittest.mock import patch
+from io import StringIO
+
+
+class TestLeafNode(TestCase):
+	def test_to_html_no_value(self):
+		node = LeafNode("p", None)
+		with self.assertRaises(ValueError):
+			html = node.to_html()
+	def test_to_html_no_tag(self):
+		node = LeafNode(None, "This is raw text")
+		self.assertEqual(node.to_html(), node.value)
+	def test_to_html_no_props(self):
+		node = LeafNode("p", "This is a paragraph with no props")
+		self.assertEqual(node.to_html(), "<p>This is a paragraph with no props</p>")
+	def test_to_html_props(self):
+		node = LeafNode("a", "This is an anchor with an href attribute", {'href': 'https://www.google.com'})
+		self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\">This is an anchor with an href attribute</a>")
+
+if __name__ == "__main__":
+	main()
